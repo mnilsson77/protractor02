9801765a85f25820a161b455476f4e4246e3cfa6
Mattias Nilsson <mnilsson77@gmail.com>
2017-10-24 21:07:13 +0200

    Mattias first protractor tryout

---
+++ spec.js
@@ -0,0 +1,15 @@
+// spec.js
+describe('Protractor Demo App', function() {
+  it('should add one and two', function() {
+    browser.get('http://juliemr.github.io/protractor-demo/');
+    element(by.model('first')).sendKeys(1);
+    element(by.model('second')).sendKeys(2);
+    browser.sleep(2000);
+
+    element(by.id('gobutton')).click();
+    browser.sleep(2000);
+    expect(element(by.binding('latest')).getText()).
+        toEqual('5'); // This is wrong!
+        browser.sleep(2000);
+  });
+});
